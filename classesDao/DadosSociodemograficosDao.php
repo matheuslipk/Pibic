085706827ab5a57f596b97bc6452e29bd57ec703
AUTOLOGON <AUTOLOGON@192.168.0.195>
2017-05-12 10:51:44 -0300

    Salvando dados Socioeconômicos

---
+++ classesDao/DadosSociodemograficosDao.php
@@ -0,0 +1,61 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/especial/ConexaoDao.php';
+
+class DadosSociodemogradicosDao {
+   public function inserirDadosSociodemogradicos($array){
+      $con = ConexaoDao::getConecao();
+      $query = "INSERT INTO dadosSociodemograficos VALUES (?,?,?,?,?,?,?,?,?,?)";
+      $stmt = $con->prepare($query);
+      $stmt->bind_param("issssssidi", $array['idProntuario'], $array['nome'], 
+              $array['dataNascimento'], $array['racaCor'], $array['escolaridade'], 
+              $array['estadoCivil'], $array['ocupacao'], $array['pessoasNaCasa'],
+              $array['rendaFamiliar'], $array['enderecoAtual']);
+      if($stmt->execute()){
+         $stmt->close();
+         $con->close();
+         return TRUE;
+      }
+      $erro = $stmt->error;
+      $stmt->close();
+      $con->close();
+      return $erro;
+   }
+   
+   public function getDadosSociodemogradicosByProntuario($prontuario){
+      $con = ConexaoDao::getConecao();
+      $query = "SELECT * FROM dadosSociodemograficos WHERE idProntuario=?";
+      $stmt = $con->prepare($query);
+      $stmt->bind_param("i", $prontuario);
+      if($stmt->execute()){
+         $result = $stmt->get_result();
+         $array = $result->fetch_assoc();
+         $stmt->close();
+         $con->close();
+         return $array;
+      }
+      $stmt->close();
+      $con->close();
+      return FALSE;
+   }
+   
+   public function updateDadosSociodemogradicos($array){      
+      $con = ConexaoDao::getConecao();
+      $query = "UPDATE dadosSociodemograficos SET nome=?, dataNascimento=?, "
+              . "racaCor=?, escolaridade=?, estadoCivil=?, ocupacao=?, "
+              . "pessoasNaCasa=?, rendaFamiliar=?, enderecoAtual=? WHERE idProntuario=?";
+      $stmt = $con->prepare($query);
+      $stmt->bind_param("issssssidi", $array['nome'], 
+              $array['dataNascimento'], $array['racaCor'], $array['escolaridade'], 
+              $array['estadoCivil'], $array['ocupacao'], $array['pessoasNaCasa'],
+              $array['rendaFamiliar'], $array['enderecoAtual'], $array['idProntuario']);
+      if($stmt->execute()){
+         $stmt->close();
+         $con->close();
+         return TRUE;
+      }
+      $erro = $stmt->error;
+      $stmt->close();
+      $con->close();
+      return $erro;
+   }
+}
--- controll/dadosSociodemograficos/inserirDadosSociodemograficos.php
+++ controll/dadosSociodemograficos/inserirDadosSociodemograficos.php
@@ -1,10 +1,20 @@
 <?php
-//require_once $_SERVER['DOCUMENT_ROOT'].'/classesDao/DadosSociodemograFicosDao.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/classesDao/DadosSociodemograFicosDao.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/classesDao/EnderecoDao.php';
 
 function inserirDadosSociodemogradicos(){
    
    $array['idProntuario'] = $_POST['prontuario'];
+   $array['nome'] = $_POST['nome'];
+   $array['dataNascimento'] = $_POST['dataNascimento'];
+   $array['racaCor'] = $_POST['racaCor'];
+   $array['escolaridade'] = $_POST['escolaridade'];
+   $array['estadoCivil'] = $_POST['estadoCivil'];
+   $array['ocupacao'] = $_POST['ocupacao'];
+   $array['pessoasNaCasa'] = $_POST['pessoasNaCasa'];
+   $array['rendaFamiliar'] = $_POST['rendaFamiliar'];
+   $array['enderecoAtual'] = $_POST['prontuario'];
+   
    $array['uf'] = $_POST['uf'];
    $array['municipio'] = $_POST['municipio'];
    $array['logradouro'] = $_POST['logradouro'];
@@ -13,25 +23,50 @@ function inserirDadosSociodemogradicos(){
    $array['telefone'] = $_POST['telefone'];
    
    $enderecoDao = new EnderecoDao();
+   $dadosDao = new DadosSociodemogradicosDao();
+   
    if(isEnderecoExiste($_POST['prontuario'])){
-      $result = $enderecoDao->updateEndereco($array);
-      echo $result;
-      if($result === TRUE){
-         header("Location: /view/entrevistaMae.php?prontuario={$array['idProntuario']}");
+      $result1 = $enderecoDao->updateEndereco($array);
+      echo $result1;
+      if($result1 === TRUE){
+         header("Location: /view/antecedentes.php?prontuario={$array['idProntuario']}");
       }
+      if(isDadosSocioExiste($_POST['prontuario'])){
+         $result = $dadosDao->updateDadosSociodemogradicos($array);
+         echo $result;
+         if($result === TRUE){
+            header("Location: /view/antecedentes.php?prontuario={$array['idProntuario']}");
+         }
+      }else{
+         $result = $dadosDao->inserirDadosSociodemogradicos($array);
+         echo $result;
+         if($result === TRUE){
+            header("Location: /view/antecedentes.php?prontuario={$array['idProntuario']}");
+         }
+      }  
    }else{
-      $result = $enderecoDao->inserirEndereco($array);
-      echo $result;
-      if($result === TRUE){
-         header("Location: /view/entrevistaMae.php?prontuario={$array['idProntuario']}");
+      $result1 = $enderecoDao->inserirEndereco($array);
+      echo $result1;
+      if($result1 === TRUE){
+         header("Location: /view/antecedentes.php?prontuario={$array['idProntuario']}");
       }
    }
-   
+      
 }
 
 function isEnderecoExiste($prontuario){
    $ederecoDao = new EnderecoDao();
    $result = $ederecoDao->getEnderecoByProntuario($prontuario);
+   if($result===FALSE){
+      return FALSE;
+   }elseif (count($result)>=1) {
+      return TRUE;
+   }
+}
+
+function isDadosSocioExiste($prontuario){
+   $dadosDao = new DadosSociodemogradicosDao();
+   $result = $dadosDao->getDadosSociodemogradicosByProntuario($prontuario);
    if($result===FALSE){
       return FALSE;
    }elseif (count($result)>=1) {
