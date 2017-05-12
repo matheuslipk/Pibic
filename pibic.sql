308e22130dec479fbc516f6eb3bf1c503dc2fc1e
AUTOLOGON <AUTOLOGON@192.168.0.195>
2017-05-12 10:54:13 -0300

    Banco atualizado

--- pibic.sql
+++ pibic.sql
@@ -1,30 +1,40 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4deb1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Tempo de geração: 11/05/2017 às 23:23
--- Versão do servidor: 5.7.18-0ubuntu0.16.10.1
--- Versão do PHP: 7.0.15-0ubuntu0.16.10.4
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Banco de dados: `pibic`
---
 
--- --------------------------------------------------------
+CREATE TABLE `dadossociodemograficos` (
+  `idProntuario` int(11) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `dataNascimento` date NOT NULL,
+  `racaCor` varchar(30) NOT NULL,
+  `escolaridade` varchar(30) NOT NULL,
+  `estadoCivil` varchar(30) NOT NULL,
+  `ocupacao` varchar(30) NOT NULL,
+  `pessoasNaCasa` int(11) NOT NULL,
+  `rendaFamiliar` double NOT NULL,
+  `enderecoAtual` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Estrutura para tabela `estado`
---
+INSERT INTO `dadossociodemograficos` VALUES
+(1, '0', '2017-05-05', '', 'Sem escolaridade', 'Solteira', 'CEO da maior empresa do mundo', 10, 50000, 1);
+
+CREATE TABLE `endereco` (
+  `idProntuario` int(11) NOT NULL,
+  `uf` char(2) NOT NULL,
+  `municipio` int(11) NOT NULL,
+  `logradouro` varchar(50) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `bairro` varchar(30) NOT NULL,
+  `telefone` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `endereco` VALUES
+(1, 'PI', 2200053, '', 0, '', '+5586994012914');
 
 CREATE TABLE `estado` (
   `id` int(11) NOT NULL,
@@ -34,46 +44,36 @@ CREATE TABLE `estado` (
   `regiao` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Fazendo dump de dados para tabela `estado`
---
-
 INSERT INTO `estado` VALUES
 (1, 12, 'Acre', 'AC', 1),
 (2, 27, 'Alagoas', 'AL', 2),
-(3, 16, 'Amapá', 'AP', 1),
+(3, 16, 'AmapÃ¡', 'AP', 1),
 (4, 13, 'Amazonas', 'AM', 1),
 (5, 29, 'Bahia', 'BA', 2),
-(6, 23, 'Ceará', 'CE', 2),
+(6, 23, 'CearÃ¡', 'CE', 2),
 (7, 53, 'Distrito Federal', 'DF', 5),
-(8, 32, 'Espírito Santo', 'ES', 3),
-(9, 52, 'Goiás', 'GO', 5),
-(10, 21, 'Maranhão', 'MA', 2),
+(8, 32, 'EspÃ­rito Santo', 'ES', 3),
+(9, 52, 'GoiÃ¡s', 'GO', 5),
+(10, 21, 'MaranhÃ£o', 'MA', 2),
 (11, 51, 'Mato Grosso', 'MT', 5),
 (12, 50, 'Mato Grosso do Sul', 'MS', 5),
 (13, 31, 'Minas Gerais', 'MG', 3),
-(14, 15, 'Pará', 'PA', 2),
-(15, 25, 'Paraíba', 'PB', 2),
-(16, 41, 'Paraná', 'PR', 4),
+(14, 15, 'ParÃ¡', 'PA', 2),
+(15, 25, 'ParaÃ­ba', 'PB', 2),
+(16, 41, 'ParanÃ¡', 'PR', 4),
 (17, 26, 'Pernambuco', 'PE', 2),
-(18, 22, 'Piauí', 'PI', 2),
+(18, 22, 'PiauÃ­', 'PI', 2),
 (19, 33, 'Rio de Janeiro', 'RJ', 3),
 (20, 24, 'Rio Grande do Norte', 'RN', 2),
 (21, 43, 'Rio Grande do Sul', 'RS', 4),
-(22, 11, 'Rondônia', 'RO', 1),
+(22, 11, 'RondÃ´nia', 'RO', 1),
 (23, 14, 'Roraima', 'RR', 1),
 (24, 42, 'Santa Catarina', 'SC', 4),
-(25, 35, 'São Paulo', 'SP', 3),
+(25, 35, 'SÃ£o Paulo', 'SP', 3),
 (26, 28, 'Sergipe', 'SE', 2),
 (27, 17, 'Tocantins', 'TO', 1);
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `exameFisico`
---
-
-CREATE TABLE `exameFisico` (
+CREATE TABLE `examefisico` (
   `idProntuario` int(11) NOT NULL,
   `peso` int(11) NOT NULL,
   `estatura` int(11) NOT NULL,
@@ -89,20 +89,10 @@ CREATE TABLE `exameFisico` (
   `outrosAchadosClinicos` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Fazendo dump de dados para tabela `exameFisico`
---
+INSERT INTO `examefisico` VALUES
+(1, 70, 60, 50, 40, 30, 20, 10, 1, 'Aparelho digestivo', 'DescriÃ§Ã£o da malformaÃ§Ã£o', 1, 'ConvulsÃµes');
 
-INSERT INTO `exameFisico` VALUES
-(1, 70, 60, 50, 40, 30, 20, 10, 1, 'Aparelho digestivo', 'Descrição da malformação', 1, 'Convulsões');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `exameHemograma`
---
-
-CREATE TABLE `exameHemograma` (
+CREATE TABLE `examehemograma` (
   `idProntuario` int(11) NOT NULL,
   `hemograma` tinyint(1) NOT NULL,
   `dataHemograma` date DEFAULT NULL,
@@ -117,13 +107,7 @@ CREATE TABLE `exameHemograma` (
   `glicose` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `exameImagem`
---
-
-CREATE TABLE `exameImagem` (
+CREATE TABLE `exameimagem` (
   `idProntuario` int(11) NOT NULL,
   `tomografiaCran` int(11) NOT NULL,
   `resultTomografiaCran` varchar(30) DEFAULT NULL,
@@ -134,23 +118,19 @@ CREATE TABLE `exameImagem` (
   `ultrassomAbd` int(11) NOT NULL,
   `resultUltrassomAbd` varchar(30) DEFAULT NULL,
   `ecocardiograma` int(11) NOT NULL,
-  `resultEcocardiograma` varchar(30) DEFAULT NULL
+  `resultEcocardiograma` varchar(30) DEFAULT NULL,
+  `fundoOlho` int(11) DEFAULT NULL,
+  `alterFundoOlho` int(11) DEFAULT NULL,
+  `descAlterFundoOlho` varchar(50) DEFAULT NULL,
+  `testeOrelhinha` int(11) DEFAULT NULL,
+  `alterTesteOrelhinha` int(11) DEFAULT NULL,
+  `descAlterTesteOrelhinha` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Fazendo dump de dados para tabela `exameImagem`
---
+INSERT INTO `exameimagem` VALUES
+(1, 1, '', 1, '', 0, '', 0, '', 1, '', 0, 1, 'esp', 0, 1, 'cificaÃ§Ã£o');
 
-INSERT INTO `exameImagem` VALUES
-(1, 0, '', 0, '', 0, '', 0, '', 1, '');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `examePuncaoLiquorica`
---
-
-CREATE TABLE `examePuncaoLiquorica` (
+CREATE TABLE `examepuncaoliquorica` (
   `idProntuario` int(11) NOT NULL,
   `puncaoLiquorica` int(11) NOT NULL,
   `dataPuncaoLiquorica` date DEFAULT NULL,
@@ -165,18 +145,8 @@ CREATE TABLE `examePuncaoLiquorica` (
   `glicose` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Fazendo dump de dados para tabela `examePuncaoLiquorica`
---
-
-INSERT INTO `examePuncaoLiquorica` VALUES
-(1, 1, '2017-05-31', 'Límpido', 0, 0, 0, 0, 0, 0, 0, 0);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `municipio`
---
+INSERT INTO `examepuncaoliquorica` VALUES
+(1, 1, '2017-05-16', 'LÃ­mpido', 0, 0, 0, 0, 0, 0, 0, 0);
 
 CREATE TABLE `municipio` (
   `codigo` int(11) NOT NULL,
@@ -184,32 +154,16 @@ CREATE TABLE `municipio` (
   `uf` char(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `prontuario`
---
-
 CREATE TABLE `prontuario` (
   `idProntuario` int(11) NOT NULL,
   `usuario` int(11) NOT NULL,
   `dataCriacao` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Fazendo dump de dados para tabela `prontuario`
---
-
 INSERT INTO `prontuario` VALUES
 (1, 100, '2017-05-06 14:42:17');
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `recemNascido`
---
-
-CREATE TABLE `recemNascido` (
+CREATE TABLE `recemnascido` (
   `idProntuario` int(11) NOT NULL,
   `dataParto` date NOT NULL,
   `sexo` varchar(20) NOT NULL,
@@ -224,20 +178,10 @@ CREATE TABLE `recemNascido` (
   `outroDano` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Fazendo dump de dados para tabela `recemNascido`
---
+INSERT INTO `recemnascido` VALUES
+(1, '2017-05-04', 'Masculino', 50, 0, 'PÃ³s-termo', 0, '1Âº Gemelar', 'Normal (Vaginal)', 0, 'AnÃ³xico', 'Outro');
 
-INSERT INTO `recemNascido` VALUES
-(1, '2017-05-04', 'Masculino', 50, 0, 'Pós-termo', 0, '1º Gemelar', 'Normal (Vaginal)', 0, 'Anóxico', 'Outro');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `servicoSaude`
---
-
-CREATE TABLE `servicoSaude` (
+CREATE TABLE `servicosaude` (
   `idProntuario` int(11) NOT NULL,
   `idTipoHospital` int(11) NOT NULL,
   `identServico` varchar(200) NOT NULL,
@@ -245,40 +189,20 @@ CREATE TABLE `servicoSaude` (
   `resp` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Fazendo dump de dados para tabela `servicoSaude`
---
+INSERT INTO `servicosaude` VALUES
+(1, 5, 'ident', 2202208, 'responsÃ¡vel');
 
-INSERT INTO `servicoSaude` VALUES
-(1, 5, 'ident', 2202208, 'responsável');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `tipoHospital`
---
-
-CREATE TABLE `tipoHospital` (
+CREATE TABLE `tipohospital` (
   `idTipoHospital` int(11) NOT NULL,
   `tipo` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Fazendo dump de dados para tabela `tipoHospital`
---
-
-INSERT INTO `tipoHospital` VALUES
-(1, 'Hospital Público'),
+INSERT INTO `tipohospital` VALUES
+(1, 'Hospital PÃºblico'),
 (2, 'Hospital Particular'),
 (3, 'Hospital particular'),
-(4, 'Domicílio'),
+(4, 'DomicÃ­lio'),
 (5, 'Outro');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `usuario`
---
 
 CREATE TABLE `usuario` (
   `idUsuario` int(11) NOT NULL,
@@ -287,161 +211,99 @@ CREATE TABLE `usuario` (
   `senha` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Fazendo dump de dados para tabela `usuario`
---
-
 INSERT INTO `usuario` VALUES
-(100, 'matheuslipk', 'Matheus Araújo de Alcantara', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+(100, 'matheuslipk', 'Matheus AraÃºjo de Alcantara', '7c4a8d09ca3762af61e59520943dc26494f8941b');
 
---
--- Índices de tabelas apagadas
---
 
---
--- Índices de tabela `estado`
---
+ALTER TABLE `dadossociodemograficos`
+  ADD PRIMARY KEY (`idProntuario`),
+  ADD KEY `enderecoAtual` (`enderecoAtual`);
+
+ALTER TABLE `endereco`
+  ADD PRIMARY KEY (`idProntuario`),
+  ADD KEY `municipio` (`municipio`),
+  ADD KEY `uf` (`uf`);
+
 ALTER TABLE `estado`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `uf` (`uf`);
 
---
--- Índices de tabela `exameFisico`
---
-ALTER TABLE `exameFisico`
+ALTER TABLE `examefisico`
   ADD PRIMARY KEY (`idProntuario`);
 
---
--- Índices de tabela `exameHemograma`
---
-ALTER TABLE `exameHemograma`
+ALTER TABLE `examehemograma`
   ADD PRIMARY KEY (`idProntuario`);
 
---
--- Índices de tabela `exameImagem`
---
-ALTER TABLE `exameImagem`
+ALTER TABLE `exameimagem`
   ADD PRIMARY KEY (`idProntuario`);
 
---
--- Índices de tabela `examePuncaoLiquorica`
---
-ALTER TABLE `examePuncaoLiquorica`
+ALTER TABLE `examepuncaoliquorica`
   ADD PRIMARY KEY (`idProntuario`);
 
---
--- Índices de tabela `municipio`
---
 ALTER TABLE `municipio`
   ADD PRIMARY KEY (`codigo`),
   ADD KEY `uf` (`uf`);
 
---
--- Índices de tabela `prontuario`
---
 ALTER TABLE `prontuario`
   ADD PRIMARY KEY (`idProntuario`),
   ADD KEY `usuario` (`usuario`);
 
---
--- Índices de tabela `recemNascido`
---
-ALTER TABLE `recemNascido`
+ALTER TABLE `recemnascido`
   ADD PRIMARY KEY (`idProntuario`);
 
---
--- Índices de tabela `servicoSaude`
---
-ALTER TABLE `servicoSaude`
+ALTER TABLE `servicosaude`
   ADD PRIMARY KEY (`idProntuario`),
   ADD KEY `idTipoHostpital` (`idTipoHospital`),
   ADD KEY `municipioOcorrencia` (`municipioOcorrencia`);
 
---
--- Índices de tabela `tipoHospital`
---
-ALTER TABLE `tipoHospital`
+ALTER TABLE `tipohospital`
   ADD PRIMARY KEY (`idTipoHospital`);
 
---
--- Índices de tabela `usuario`
---
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`idUsuario`),
   ADD UNIQUE KEY `nick` (`nick`);
 
---
--- AUTO_INCREMENT de tabelas apagadas
---
 
---
--- AUTO_INCREMENT de tabela `estado`
---
 ALTER TABLE `estado`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
---
--- AUTO_INCREMENT de tabela `tipoHospital`
---
-ALTER TABLE `tipoHospital`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tipohospital`
   MODIFY `idTipoHospital` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT de tabela `usuario`
---
 ALTER TABLE `usuario`
   MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
---
--- Restrições para dumps de tabelas
---
 
---
--- Restrições para tabelas `exameFisico`
---
-ALTER TABLE `exameFisico`
+ALTER TABLE `dadossociodemograficos`
+  ADD CONSTRAINT `dadossociodemograficos_ibfk_1` FOREIGN KEY (`idProntuario`) REFERENCES `prontuario` (`idProntuario`),
+  ADD CONSTRAINT `dadossociodemograficos_ibfk_2` FOREIGN KEY (`enderecoAtual`) REFERENCES `endereco` (`idProntuario`);
+
+ALTER TABLE `endereco`
+  ADD CONSTRAINT `endereco_ibfk_2` FOREIGN KEY (`municipio`) REFERENCES `municipio` (`codigo`),
+  ADD CONSTRAINT `endereco_ibfk_4` FOREIGN KEY (`idProntuario`) REFERENCES `prontuario` (`idProntuario`),
+  ADD CONSTRAINT `endereco_ibfk_5` FOREIGN KEY (`uf`) REFERENCES `estado` (`uf`);
+
+ALTER TABLE `examefisico`
   ADD CONSTRAINT `exameFisico_ibfk_1` FOREIGN KEY (`idProntuario`) REFERENCES `prontuario` (`idProntuario`);
 
---
--- Restrições para tabelas `exameHemograma`
---
-ALTER TABLE `exameHemograma`
+ALTER TABLE `examehemograma`
   ADD CONSTRAINT `exameHemograma_ibfk_1` FOREIGN KEY (`idProntuario`) REFERENCES `prontuario` (`idProntuario`);
 
---
--- Restrições para tabelas `exameImagem`
---
-ALTER TABLE `exameImagem`
+ALTER TABLE `exameimagem`
   ADD CONSTRAINT `exameImagem_ibfk_1` FOREIGN KEY (`idProntuario`) REFERENCES `prontuario` (`idProntuario`);
 
---
--- Restrições para tabelas `examePuncaoLiquorica`
---
-ALTER TABLE `examePuncaoLiquorica`
+ALTER TABLE `examepuncaoliquorica`
   ADD CONSTRAINT `examePuncaoLiquorica_ibfk_1` FOREIGN KEY (`idProntuario`) REFERENCES `prontuario` (`idProntuario`);
 
---
--- Restrições para tabelas `municipio`
---
 ALTER TABLE `municipio`
   ADD CONSTRAINT `municipio_ibfk_1` FOREIGN KEY (`uf`) REFERENCES `estado` (`uf`);
 
---
--- Restrições para tabelas `prontuario`
---
 ALTER TABLE `prontuario`
   ADD CONSTRAINT `prontuario_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`idUsuario`);
 
---
--- Restrições para tabelas `recemNascido`
---
-ALTER TABLE `recemNascido`
+ALTER TABLE `recemnascido`
   ADD CONSTRAINT `recemNascido_ibfk_1` FOREIGN KEY (`idProntuario`) REFERENCES `prontuario` (`idProntuario`);
 
---
--- Restrições para tabelas `servicoSaude`
---
-ALTER TABLE `servicoSaude`
-  ADD CONSTRAINT `servicoSaude_ibfk_1` FOREIGN KEY (`idProntuario`) REFERENCES `prontuario` (`idProntuario`),
-  ADD CONSTRAINT `servicoSaude_ibfk_2` FOREIGN KEY (`idTipoHospital`) REFERENCES `tipoHospital` (`idTipoHospital`),
-  ADD CONSTRAINT `servicoSaude_ibfk_3` FOREIGN KEY (`municipioOcorrencia`) REFERENCES `municipio` (`codigo`);
+ALTER TABLE `servicosaude`
+  ADD CONSTRAINT `servicosaude_ibfk_1` FOREIGN KEY (`idTipoHospital`) REFERENCES `tipohospital` (`idTipoHospital`),
+  ADD CONSTRAINT `servicosaude_ibfk_2` FOREIGN KEY (`municipioOcorrencia`) REFERENCES `municipio` (`codigo`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
